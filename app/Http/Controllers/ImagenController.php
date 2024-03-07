c2841e1697f717238d44118dfd80bbbdb245c2f0
Jeff Rios <jeff_rios2002@outlook.com>
2024-03-06 20:01:14 -0500

    Guarado de imagen

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
 
 class ImagenController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+
+        $imagen = $request->file('file');
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $imagenServidor = Image::read($imagen);
+        $imagenServidor->cover(width: 1000, height:1000);
+
+        $imagenPath = public_path("uploads"). '/' .$nombreImagen;
+        $imagenServidor->save($imagenPath);
+        return response()->json(['image'=> $nombreImagen]);
+    }
 }
