13aea74858de8f2da465b44c45c7a7e69805329d
Jeff Rios <jeff_rios2002@outlook.com>
2024-03-06 20:01:05 -0500

    Creando y configurando modelo, controllador de Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -19,9 +20,33 @@ class PostController extends Controller
             "user" => $user
         ]);
     }
-
     public function create()
     {
         return view('posts.create');
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title'=> 'required|max:255',
+            'description' => 'required',
+            'image' => 'required',
+        ],
+        [
+            'title.required' => 'No te olvides del titulo',
+            'description.required' => 'No te olvides de agregar una descripción',
+            'image.required' => 'No te olvides de colocar tu imagen', 
+        ]);
+
+        Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'imagen' => $request->image,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect() -> route('posts.index', auth()->user()->username);
+        
+    }
+
 }
