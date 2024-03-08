d262dc7d28f902dc46e2e8c857b70474b9c78cad
Jeff Rios <jeff_rios2002@outlook.com>
2024-03-08 18:44:50 -0500

    Agregar los estilos de paginacion en en config de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
