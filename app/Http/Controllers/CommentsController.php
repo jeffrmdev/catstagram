46ff371688079ce365a4333941d6977bc1715e47
Jefferson Rios <jeff_rios2002@outlook.com>
2024-04-09 17:27:45 -0500

    Creación de modelo y tabla de comentarios

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    //
+    public function store(Request $request, User $user, Post $post)
+    {
+        #Validar
+        $this->validate($request, [
+            'comment' => 'required|max:255',
+        ]);
+
+        #Almacenar
+        Comment::create([
+          'comment' =>  $request->comment,
+          'user_id' => auth()->user()->id,
+          'post_id' => $post->id
+        ]);
+
+
+        #Imprimir mensaje
+        return back()->with('message','Comentario publicado');
+    }
+}
