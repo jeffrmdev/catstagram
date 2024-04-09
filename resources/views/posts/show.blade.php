a93c9e32722b3c611febefabc572bad4a2af5223
Jefferson Rios <jeff_rios2002@outlook.com>
2024-04-09 17:29:04 -0500

    Mostrar comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
 @section('contenido')
 
 <div class="container mx-auto flex">
-    <div class="w-1/2 flex items-center flex-col justify-stretch">
+    <div class="w-1/4 flex items-center flex-col justify-stretch">
         <div class="flex flex-row items-start">
             <p class="font-bold mx-2 my-1"> {{ $post->user->username }}</p>
             <span class="font-extralight mx-2 my-1"> - </span>
@@ -17,7 +17,26 @@
                 {{ $post->created_at->diffForHumans() }}
             </p>
         </div>
-        <img class="w-1/3" src="{{ asset('uploads') . '/' . $post->imagen}}" alt="Imagen del post {{ $post->title }}">
+        <img class="p-2" src="{{ asset('uploads') . '/' . $post->imagen}}" alt="Imagen del post {{ $post->title }}">
+        <div>
+
+            @auth
+            @if ($post->user_id === auth()->user()->id)
+                
+            <form method="POST" action=" {{ route('posts.destroy', $post )}} ">
+                @method('DELETE')
+                @csrf
+                <input 
+                    type="submit" 
+                    value="Eliminar publicacion"
+                    class="p-4 bg-red-800 text-white cursor-pointer"
+                />
+            </form>
+            @endif               
+            @endauth
+
+        </div>
+
         <div class="flex flex-row justify-stretch text-left m-auto font-extralight">
             <p class="m-5">0 likes</p>
             <p class="m-5">0 comentarios</p>
@@ -29,12 +48,18 @@
         </div>
     </div>
     @auth
-    <div class="w-1/2">
+    <div class="w-1/4">
         <p>Comentarios</p>
         <div>
-            <form action="">
+            <form method="POST" action=" {{ route('comments.store', ['post' => $post, 'user' => $user]) }}">
+                @csrf
                 <div class="mx-7 mt-7 text-left">
                     <label for="comment" class="block font-light mb-2">Agregar un comentario</label>
+                    @if (session('message'))
+                        <div>
+                            {{ session ('message') }}
+                        </div>
+                    @endif
                     <textarea 
                         name="comment" 
                         id="comment"
@@ -60,8 +85,19 @@
         </div>
     </div>
     @endauth
-    <div>
+    <div class="w-1/2">
         <p>Lista de comentarios</p>
+        @if ($post->comments->count())
+            @foreach ($post->comments as $comment)
+                <div class="m-5 p-2">
+                    <a href="{{ route('posts.index', $comment->user) }}">
+                        {{ $comment->user->username}}
+                    </a>
+                    <p>{{ $comment->comment }}</p>
+                    <p class="font-extralight">{{ $comment->created_at->diffForHumans()}}</p>
+                </div>
+            @endforeach
+        @endif
     </div>
 </div>
 
