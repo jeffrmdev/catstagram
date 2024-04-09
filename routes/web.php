1cf8af58392497132bde29e87a4e6cea3b69a98a
Jefferson Rios <jeff_rios2002@outlook.com>
2024-04-09 17:29:17 -0500

    Cambio

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentsController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
@@ -26,11 +27,16 @@ Route::post('/logout', [LogoutController::class, 'store']) -> name('logout');
 
 
 
-//Enlace usuario
+//Enlaces del usuario
 Route::get('/{user:username}', [PostController::class, 'index']) -> name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create']) -> name('posts.create');
 Route::post('/posts', [PostController::class, 'store']) -> name('posts.store');
 Route::get('/{user:username}/post/{post}', [PostController::class, 'show']) -> name('posts.show');
+Route::delete('/posts/{post}', [PostController::class,'destroy']) -> name('posts.destroy');
+
+
+#Seccion de comentarios
+Route::post('/{user:username}/post/{post}', [CommentsController::class, 'store']) -> name('comments.store');
 
 
 Route::post('/imagenes', [ImagenController::class, 'store']) -> name('imagen.store');
