c8dd115871bb976c1f3ffce4d7efcb9c30d092ea
Jefferson Rios <jeff_rios2002@outlook.com>
2024-04-27 10:21:21 -0500

    Mejroa de diseño en esta página

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Registrarse en CatStagram
 @endsection
 
 @section('contenido')
-    <div class="md:flex md:justify-center md:gap-6 md:items-center">
-        <div class="md:w-2/5 shadow-md">
+    <div class="md:flex justify-center">
+        <div class="w-1/3 shadow-md">
             <img src="{{asset('img/registro.jpg')}}" 
                  alt="foto de perfil de un gato"
-                 class="w-full h-full object-cover object-center">
+                 class="w-full h-full object-cover">
         </div>
 
         <div class="md:w-1/2 bg-white p-10 shadow-lg">
+            <h1 class="text-3xl mb-5 text-left">Registrate en <strong>Catstagram</strong></h1>
             <form action="{{ route('register') }}" method="POST">
                 @csrf
-                <div class="mb-4 flex">
+                <div class="mt-10 flex">
                     <div class="w-1/2 mr-2">
-                        <label for="name" class="block font-light mb-2">Nombre:</label>
+                        <label for="name" class="text-left block font-light mb-2">Nombre:</label>
                         <input 
                             type="text" 
                             id="name" 
                             name="name" 
-                            placeholder="Tu nombre aquí"
-                            class="@error('name') border-red-600 @enderror placeholder:italic w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
+                            placeholder="Aquí tu nombre"
+                            class="@error('name') border-red-600 @enderror w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
                             value="{{ old('name') }}"
                        />
                         @error('name')
@@ -31,13 +31,13 @@
                         @enderror
                     </div>
                     <div class="w-1/2 ml-2">
-                        <label for="lastname" class="block font-light mb-2">Apellido:</label>
+                        <label for="lastname" class="text-left block font-light mb-2">Apellido:</label>
                         <input 
                             type="text" 
                             id="lastname" 
                             name="lastname"
                             placeholder="También tu apellido"
-                            class="@error('lastname') border-red-600 @enderror placeholder:italic w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
+                            class="@error('lastname') border-red-600 @enderror w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
                             value="{{ old('lastname') }}"
                         />
                         @error('lastname')
@@ -45,28 +45,28 @@
                         @enderror 
                     </div>
                 </div>
-                <div class="">
-                    <label for="username" class="block font-light mb-2">Nombre de usuario:</label>
+                <div class="mt-4">
+                    <label for="username" class="text-left block font-light mb-2">Nombre de usuario:</label>
                     <input 
                         type="text" 
                         id="username" 
                         name="username"
                         placeholder="Puede ser un apodo genial"
-                        class="@error('username') border-red-600 @enderror placeholder:italic w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
+                        class="@error('username') border-red-600 @enderror w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
                         value="{{ old('username') }}"
                     />
                     @error('username')
                             <p class="text-red-600">{{ $message }}</p>
                         @enderror
                 </div>
-                <div class=" mt-4">
-                    <label for="email" class="block font-light mb-2">Correo:</label>
+                <div class="mt-4">
+                    <label for="email" class="text-left block font-light mb-2">Correo:</label>
                     <input 
                         type="text" 
                         id="email" 
                         name="email"
                         placeholder="Tu correo electrónico"
-                        class="@error('email') border-red-600 @enderror placeholder:italic w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
+                        class="@error('email') border-red-600 @enderror w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
                         value="{{ old('email') }}"
                     />
                     @error('email')
@@ -75,26 +75,26 @@
                 </div>
                 <div class="mb-4 flex mt-4">
                     <div class="w-1/2 mr-2">
-                        <label for="password" class="block font-light mb-2">Contraseña:</label>
+                        <label for="password" class="text-left block font-light mb-2">Contraseña:</label>
                         <input 
                             type="password" 
                             id="password" 
                             name="password" 
                             placeholder="Tu contraseña aquí"
-                            class="@error('password') border-red-600 @enderror placeholder:italic w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
+                            class="@error('password') border-red-600 @enderror w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
                         />
                         @error('password')
                             <p class="text-red-600">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="w-1/2 ml-2">
-                        <label for="password_confirmation" class="block font-light mb-2">Confirma tu contraseña:</label>
+                        <label for="password_confirmation" class="text-left block font-light mb-2">Confirma tu contraseña:</label>
                         <input 
                             type="password" 
                             id="password_confirmation" 
                             name="password_confirmation"
                             placeholder="Valida tu contraseña"
-                            class="@error('password') border-red-600 @enderror placeholder:italic w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
+                            class="@error('password') border-red-600 @enderror w-full border-b-2 bg-white border-gray-400 focus:outline-none focus:border-gray-950 p-2"
                         />
                         @error('password')
                             <p class="text-red-600">{{ $message }}</p>
