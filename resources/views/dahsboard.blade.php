eccf0fa7d24a57fe4ab3b6c1fab49026fd7976f2
Jefferson Rios <jeff_rios2002@outlook.com>
2024-07-02 16:58:50 -0500

    Se arregla el diseño

--- resources/views/dahsboard.blade.php
+++ resources/views/dahsboard.blade.php
@@ -6,15 +6,15 @@
 
 @section('contenido')
 
-    <div class="flex justify-center items-center text-center">
+    <div class="w-2/5 flex justify-center items-center text-center ms-52">
         <div class="w-fit max-h-full grid grid-cols-2 gap-2 justify-between">
             <div class="flex flex-col items-center col-span-2 md:col-span-1">
                 <img class="img h-52 -m-2" src="{{ asset('img/usercat.svg')}}" alt="Imagen Usuario"/>
-                <button onclick="alert('hola mundo')" class="h-fit w-fit p-2 bg-slate-500 text-white transition-colors hover:bg-slate-700">Cambiar imagen</button>
+                <button onclick="alert('hola mundo')" class="border rounded-full p-2 px-3 mx-1 transition-colors bg-teal-950 text-gray-50 font-light hover:bg-teal-700">Cambiar imagen</button>
             </div>
             <div class="col-span-2 md:col-span-1 w-full px-5 flex flex-col text-center md:text-left">
                 <p class="p-2 ps-0 font-medium text-3xl text-gray-800">{{ $user -> username }}</p>
-                <div class="grid md:grid-cols-3 gap-2">
+                <div class="grid gap-2">
                     <div class="">
                         <span class="font-light">0</span>
                         <span class="text-gray-800 text-sm mb-3 font-medium">
@@ -52,7 +52,7 @@
         </div>
     </div>
 
-    <section class="container w-full h-full">
+    <section class="container w-3/4 h-full mx-10">
 
         @if($posts->count())
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,24 +12,37 @@
 
 <body class="bg-gray-100">
     <header class="p-5 border-b bg-white shadow-inner">
-        <div class="flex justify-between">
-            <h1 class="text-3xl m-auto font-bold flex ">
-                <a href="/" class="flex flex-row-reverse content-center"><span>Catstragram</span>
+        <div class="flex justify-items-stretch">
+            <div class="text-3xl font-bold flex m-auto">
+                <a href="/" class="flex flex-row-reverse"><span>Catstragram</span>
                 <img class="h-full" src="{{ asset('img/icons/icon_cat_catstagram.svg')}}" alt="Icono de Catstagram"></a>
-            </h1>
+            </div>
             @auth
-
-                <div class="box-content">         
-                    <a href="{{ route('posts.create') }}" class="m-auto flex flex-col p-2 align-middle items-center transition hover:bg-slate-200">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                          </svg> 
-                       Crear
+                <div class="box-content flex m-auto">         
+                    <a href="{{ route('posts.create') }}" class="m-auto flex flex-col p-2 uppercase align-middle items-center transition hover:bg-slate-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-6 h-6"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#000000" d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg>
+                          <span class="text-xs">Crear</span> 
+                    </a>
+                    <a class="m-auto flex flex-col p-2 uppercase align-middle items-center transition hover:bg-slate-200" href="{{ route("posts.index", [$user = Auth::user()]) }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-6 h-6"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+                        <span class="text-xs">Perfil</span> 
+                    </a>
+                    <a class="m-auto flex flex-col p-2 uppercase align-middle items-center transition hover:bg-slate-200" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-6 h-6"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#000000" d="M320 192h17.1c22.1 38.3 63.5 64 110.9 64c11 0 21.8-1.4 32-4v4 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V339.2L280 448h56c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-53 0-96-43-96-96V192.5c0-16.1-12-29.8-28-31.8l-7.9-1c-17.5-2.2-30-18.2-27.8-35.7s18.2-30 35.7-27.8l7.9 1c48 6 84.1 46.8 84.1 95.3v85.3c34.4-51.7 93.2-85.8 160-85.8zm160 26.5v0c-10 3.5-20.8 5.5-32 5.5c-28.4 0-54-12.4-71.6-32h0c-3.7-4.1-7-8.5-9.9-13.2C357.3 164 352 146.6 352 128v0V32 12 10.7C352 4.8 356.7 .1 362.6 0h.2c3.3 0 6.4 1.6 8.4 4.2l0 .1L384 21.3l27.2 36.3L416 64h64l4.8-6.4L512 21.3 524.8 4.3l0-.1c2-2.6 5.1-4.2 8.4-4.2h.2C539.3 .1 544 4.8 544 10.7V12 32v96c0 17.3-4.6 33.6-12.6 47.6c-11.3 19.8-29.6 35.2-51.4 42.9zM432 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm48 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/></svg>
+                        <span class="text-xs">Explorar</span> 
+                    </a>
+                    <a class="m-auto flex flex-col p-2 uppercase align-middle items-center transition hover:bg-slate-200" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-6 h-6"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#000000" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                        <span class="text-xs">Buscar</span> 
+                    </a>
+                    <a class="m-auto flex flex-col p-2 uppercase align-middle items-center transition hover:bg-slate-200" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-6 h-6"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v25.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm0 96c61.9 0 112 50.1 112 112v25.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V208c0-61.9 50.1-112 112-112zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"/></svg>
+                        <span class="text-xs">Notificaciones</span> 
                     </a>
                 </div>
 
-                <nav class="flex align-middle">
-                    <a class="p-2 text-sm uppercase align-middle font-light" href="{{ route("posts.index", [$user = Auth::user()]) }}">Mi Perfil</a>
+                <nav class="flex m-auto">
+                    
                     <form action="{{ route('logout') }}" method="POST" class="flex align-middle p-0 m-0 text-sm uppercase font-light">
                         @csrf
                         <button type="submit" class="p-2 uppercase" href="{{ route('logout') }}"/> Cerrar sesión </a>
